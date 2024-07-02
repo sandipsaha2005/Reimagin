6fcb2b69112f796ae9e430200a388c601db7fe0c
MRWICKJ <shubhenduhalder4@gmail.com>
2024-07-02 15:34:44 +0530

    Sticky scroll data push and coustomize for 1

--- Reimagine/src/component/StickyScrollComp.jsx
+++ Reimagine/src/component/StickyScrollComp.jsx
@@ -5,48 +5,66 @@ import { StickyScroll } from "../ui/StickyScroll";
 
 const content = [
   {
-    title: "Collaborative Editing",
+    title: "Automatic Deploys",
     description:
-      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      "Your app is automatically updated on every push, with zero downtime.",
     content: (
       <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-        Collaborative Editing
-      </div>
-    ),
-  },
-  {
-    title: "Real time changes",
-    description:
-      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
-    content: (
-      <div className="h-full w-full flex items-center justify-center text-white">
-        <img
-          src="/linear.webp"
-          width={300}
-          height={300}
-          className="h-full w-full object-cover"
-          alt="linear board demo"
+        <img 
+        src="https://cdn.sanity.io/images/hvk0tap5/production/108a2a3a856d5f5fdaf5cd7b4f448734df95ee5f-50x50.svg?w=100&fit=max&auto=format" 
+        height={300}
+        width={350}
+        alt="image not found" 
+        className=" object-cover"
         />
       </div>
     ),
   },
   {
-    title: "Version control",
+    title: "Native Language Runtimes",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
-        Version control
+      <div className="h-full w-full flex items-center justify-center text-white">
+       <img 
+        src="https://cdn.sanity.io/images/hvk0tap5/production/d6789c982c36fa550c1623b4f0168d39f4fc9999-50x50.svg?w=100&fit=max&auto=format" 
+        height={300}
+        width={350}
+        alt="image not found" 
+        className=" object-cover"
+        />
       </div>
     ),
   },
   {
-    title: "Running out of content",
+    title: "Enterprise-Grade Data Stores",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "Provision highly available PostgreSQL databases with point-in-time recovery. Create persistent caches with managed Redis.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
+       <img 
+        src="https://cdn.sanity.io/images/hvk0tap5/production/3efc8cc52efc8261e522e865a19c5e119a4c7046-50x50.svg?w=100&fit=max&auto=format" 
+        height={300}
+        width={350}
+        alt="image not found" 
+        className=" object-cover"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Private Network",
+    description:
+      "Communicate securely across services using any protocol, without traversing the public Internet.",
     content: (
       <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-        Running out of content
+         <img 
+        src="https://cdn.sanity.io/images/hvk0tap5/production/d07d06566246a418bbf69719ec796da979ae132a-50x50.svg?w=100&fit=max&auto=format" 
+        height={300}
+        width={350}
+        alt="image not found" 
+        className=" object-cover"
+        />
       </div>
     ),
   },
@@ -54,7 +72,7 @@ const content = [
 
 export function StickyScrollRevealDemo() {
   return (
-    <div className="p-10">
+    <div className="p-5">
       <StickyScroll content={content} />
     </div>
   );
