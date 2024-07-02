16c6975828f44da0c38213d65cdae66e734a768f
Sandip Saha <110690069+sandipsaha2005@users.noreply.github.com>
2024-07-02 22:33:23 +0530

    scrolling card

--- Reimagine/src/ui/UseTab.jsx
+++ Reimagine/src/ui/UseTab.jsx
@@ -7,6 +7,7 @@ import {DeployCard} from '../component/Card'
 import {UpdateCard} from '../component/Card'
 // import {ThreeDCardDemo} from '../component/3dCard'
 import { ThreeDCardDemo } from '../component/3dCard'
+import { TypewriterEffectSmoothDemo } from '../component/FloatingTextComp'
 export function TabsDemo() {
   const tabs = [
     {
@@ -45,7 +46,8 @@ export function TabsDemo() {
   return (
     <>
      <p className="dark:text-white text-neutral-600 text-center text-6xl mt-10 font-bold">
-         <span  className="text-purple-900">Click,</span><span className="text-red-900">click,</span><span className="text-blue-900">done.</span>
+          <TypewriterEffectSmoothDemo/>
+         {/* <span  className="text-purple-900">Click,</span><span className="text-red-900">click,</span><span className="text-blue-900">done.</span> */}
       </p>
     <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-3xl mx-auto w-full items-start justify-start mt-16 mb-40">
       <Tabs tabs={tabs} />
