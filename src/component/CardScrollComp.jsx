646513e456c87ae745ef7353c81751a070b391c5
MRWICKJ <shubhenduhalder4@gmail.com>
2024-07-07 14:30:41 +0530

    Change Crad hover

--- src/component/CardScrollComp.jsx
+++ src/component/CardScrollComp.jsx
@@ -57,7 +57,7 @@ export function CanvasRevealEffectDemo() {
         title={`${number} Live Services`}
         onMouseEnter={() => handleHover(setNumber, number, 1000000, 50000)}
         onMouseLeave={handleMouseLeave}
-        icon={<FaUsers className="text-8xl font-bold text-white" />}
+        icon={<FaUsers className="text-8xl font-bold text-white mt-6"/>}
       >
         <CanvasRevealEffect animationSpeed={5.1} containerClassName="bg-purple-900" />
       </Card>
@@ -65,18 +65,18 @@ export function CanvasRevealEffectDemo() {
         title={`${number1} Request Per Months`}
         onMouseEnter={() => handleHover(setNumber1, number1, 40000000000, 1000000000)}
         onMouseLeave={handleMouseLeave}
-        icon={<FaCodePullRequest className="text-8xl font-bold text-white" />}
+        icon={<FaCodePullRequest className="text-8xl font-bold text-white mt-10" />}
       >
         <CanvasRevealEffect animationSpeed={5.1} containerClassName="bg-red-800" />
-        <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+        {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
       </Card>
       <Card
         title={`${number2} Deploys`}
         onMouseEnter={() => handleHover(setNumber2, number2, 40000000, 1000000)}
         onMouseLeave={handleMouseLeave}
-        icon={<GrDeploy className="text-8xl font-bold text-white" />}
+        icon={<GrDeploy className="text-8xl font-bold text-white mt-6" />}
       >
-        <CanvasRevealEffect animationSpeed={5.1} containerClassName="bg-blue-900" />
+        <CanvasRevealEffect animationSpeed={5.1} containerClassName="bg-blue-900 " />
       </Card>
     </div>
   );
@@ -95,7 +95,7 @@ const Card = ({ title, icon, onMouseEnter, onMouseLeave, children }) => {
         setHovered(false);
         onMouseLeave();
       }}
-      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-sm w-full mx-auto p-4 h-[30rem] relative"
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2] max-w-sm w-full mx-auto p-4 h-[20rem] relative"
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
